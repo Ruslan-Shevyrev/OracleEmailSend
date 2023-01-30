4fe21afdd6024d3a1d0a5a51248aa64e7c134512
Ruslan Shevyrev <ruslan.shevyrev@gmail.com>
2023-01-30 09:33:13 +0300

    add PKG_EMAIL_(SPECIFICATION).sql

--- PKG_EMAIL_(SPECIFICATION).sql
+++ PKG_EMAIL_(SPECIFICATION).sql
@@ -9,16 +9,17 @@ nsmtp_port		NUMBER:=25;
 TYPE attach_info IS RECORD (attach_name		VARCHAR2(40),
 							data_type		VARCHAR2(40) DEFAULT 'text/plain',
 							attach_content	BLOB);
+
 TYPE attachments IS TABLE OF attach_info;
 
-PROCEDURE SEND_MAIL(p_to			IN VARCHAR2,--,
+PROCEDURE SEND_MAIL(p_to			IN VARCHAR2, --,
 					p_from			IN VARCHAR2,
 					p_subject		IN VARCHAR2,
-					p_msg  			IN clob, --chr(10)||chr(13) OR UTL_TCP.crlf EVERY 1000 char
+					p_msg			IN clob, --chr(10)||chr(13) OR UTL_TCP.crlf EVERY 1000 char
 					p_charset		IN varchar2 DEFAULT vcharset,
-					p_content_type	IN VARCHAR2 DEFAULT vcontent_type, 
-					p_smtp_host 	IN VARCHAR2 DEFAULT vsmtp_host,
-					p_smtp_port 	IN NUMBER DEFAULT nsmtp_port,
+					p_content_type	IN VARCHAR2 DEFAULT vcontent_type,
+					p_smtp_host		IN VARCHAR2 DEFAULT vsmtp_host,
+					p_smtp_port		IN NUMBER DEFAULT nsmtp_port,
 					p_attach		IN attachments DEFAULT NULL);
 				
 END PKG_EMAIL;
